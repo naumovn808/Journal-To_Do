e256bb78edcdc42bdc7b3e1aa41497ffe2b43563
Nikita Naumov <naumovnn808@gmail.com>
2024-02-01 19:12:39 +0500

    change vite.config

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function App() {
 			setItems([...mapItems(items), {
 				...item,
 				date: new Date(item.date),
-				id: items.length > 0 ? Math.max(...items.map(i => i.id)) + 1 : 1
+				id: Date().toString().slice(8)
 			}]);
 		} else {
 			setItems([...mapItems(items).map(i => {
--- src/components/Logotype/Logotype.jsx
+++ src/components/Logotype/Logotype.jsx
@@ -1,11 +1,10 @@
 import styles from './Logotype.module.css';
-import logo from '/logo.svg';
 
 export default function Logotype() {
 	return (
 		<>
 			<a>
-				<img className={styles.logo} src={logo} width={180} height={25} alt='logotype'></img>
+				<img className={styles.logo} src='/logo.svg' width={180} height={25} alt='logotype'></img>
 			</a>
 		</>
 	);
