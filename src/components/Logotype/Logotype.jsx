0d680bff6b67ac800f8d392a4ab9687e0afde78f
Nikita Naumov <naumovnn808@gmail.com>
2024-02-01 20:07:50 +0500

    fix: fix logotype

--- src/components/Logotype/Logotype.jsx
+++ src/components/Logotype/Logotype.jsx
@@ -3,9 +3,7 @@ import styles from './Logotype.module.css';
 export default function Logotype() {
 	return (
 		<>
-			<a>
-				<img className={styles.logo} src='/logo.svg' width={180} height={25} alt='logotype'></img>
-			</a>
+			<img className={styles.logo} src='./logo.svg' width={180} height={25} alt='logotype'></img>
 		</>
 	);
 }
